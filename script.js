6ed215fed3fc2a7fb7d994bee5e88884e2377c57
Cody <110630101+CodySaal@users.noreply.github.com>
2022-09-16 22:29:06 -0500

    Added variables that contain all the options for characters

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+
+var numbers = "0123456789"
+
+var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +23,28 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// An Idea Function
+// function generatePassword() {
+//   var characterCount = prompt("How many characters would you like your new password to be?", "Must be between 8 and 128 characters")
+
+//   if (characterCount < 8 || characterCount > 128) {
+//     alert("Please select a valid password length");
+//   } else {
+//     characterSelection();
+//   }
+
+//   if (confirm() === true) {
+
+//   }
+
+// }
+
+// function characterSelection() {
+
+// }
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
