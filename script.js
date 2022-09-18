bb386cc6d087bbc5a54745418fd2177a72f327dc
Cody <110630101+CodySaal@users.noreply.github.com>
2022-09-17 22:11:41 -0500

    Prevented string values from going into theinitial password length prompt.

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var passwordBlueprint = ""
 
 var characterPool = ""
 
- // Try this variable
+ // Stores the amount of characters selected during the confirm prompts
  var confirmedCharacters = 0
 
 // Write password to the #password input
@@ -34,7 +34,7 @@ function characterSelection() {
   var includeNumbers = confirm ("Would you like to include numbers in your password?")
 
   var includeSpecialCharacters = confirm ("Would you like to include special characters in your password?");
-
+  //These two lines allow the user to generate multiple passwords without having to refresh the page.
   confirmedCharacters = 0;
   passwordBlueprint = "";
 
@@ -96,8 +96,11 @@ function generatePassword() {
   var characterCount = prompt("How many characters would you like your new password to be?", "Must be between 8 and 128 characters")
 
   console.log(characterCount)
-
-  if (characterCount < 8 || characterCount > 128) {
+  if (isNaN(characterCount)) {
+    alert("You must input a number value.")
+    return("Please press \"Generate Password\" button again.")
+  }
+  else if (characterCount < 8 || characterCount > 128) {
     alert("Please select a valid password length");
 
     return "A safe password is between 8 and 128 characters. Please press \"Generate Password\" button again."
